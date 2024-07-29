9a9c1c257e12e425ea544db590069022cc24db21
apku868a--tu-dresden.de <apurv.kulkarni@tu-dresden.de>
2024-07-29 16:49:23 +0200

    changed to json database approach

--- utils/data.py
+++ utils/data.py
@@ -1,31 +1,30 @@
 import streamlit as st
+import json
 
 # Input data for the application
-def available_models():
+class Database():
+    def __init__(self):
+        self.load_database()
     
-    lang_model_list_dict = {
-        "none":[],
-        "en":{
-            "en_vctkVits":{"path":"tts_models/en/vctk/vits"},
-            "en_ljspeechGlowtts":{"path":"tts_models/en/ljspeech/glow-tts"},
-            "en_ljspeechFastPitch":{"path":"tts_models/en/ljspeech/fast_pitch"},
-            "multi_xttsV2":{"path":"tts_models/multilingual/multi-dataset/xtts_v2"},
-            },
-        "de":{
-            "de_thorstenVits":{"path":"tts_models/de/thorsten/vits"},
-            "de_fairseqVits":{"path":"tts_models/deu/fairseq/vits"},
-            "multi_xttsV2":{"path":"tts_models/multilingual/multi-dataset/xtts_v2"},
-            },
-        }
+    def load_database(self):
+        f = open('./utils/data.json')
+        self.database = json.load(f)
+        f.close()
+
+    def list_languages(self):
+        lang_list_tmp = [ model_i['lang'] for model_i in self.database.values() ]
+        lang_list = list(set([item for sublist in lang_list_tmp for item in (sublist if isinstance(sublist, list) else [sublist])]))
+        lang_list.sort()
+        return lang_list
     
-    all_model_list_dict = {}
-    for lang,models in lang_model_list_dict.items():
-        all_model_list_dict.update(models)
-        for model_i_name,data in all_model_list_dict.items():
-            all_model_list_dict[model_i_name]["name"] = model_i_name
-            all_model_list_dict[model_i_name]["lang"] = lang
+    def get_models(self, language):
+        return [ key for key,value in self.database.items() if value['lang'] == language or language in value['lang'] ]
     
-    return lang_model_list_dict, all_model_list_dict
+    def list_all_models(self):
+        return list(self.database.keys())
+    
+    def get_model_data(self, model: str):
+        return self.database[model]
 
 class AppFlags:
     def __init__(self):
@@ -41,7 +40,6 @@ class AppFlags:
         if "selected_language" not in st.session_state.keys():
             st.session_state["selected_language"] = ""
 
-
 # XTTSV2 speakerlist
 ########################
 #['Claribel Dervla', 'Daisy Studious', 'Gracie Wise', 'Tammie Ema', 'Alison Dietlinde', 'Ana Florence', 'Annmarie Nele', 'Asya Anara', 'Brenda Stern', 'Gitta Nikolina', 'Henriette Usha', 'Sofia Hellen', 'Tammy Grit', 'Tanja Adelina', 'Vjollca Johnnie', 'Andrew Chipper', 'Badr Odhiambo', 'Dionisio Schuyler', 'Royston Min', 'Viktor Eka', 'Abrahan Mack', 'Adde Michal', 'Baldur Sanjin', 'Craig Gutsy', 'Damien Black', 'Gilberto Mathias', 'Ilkin Urbano', 'Kazuhiko Atallah', 'Ludvig Milivoj', 'Suad Qasim', 'Torcull Diarmuid', 'Viktor Menelaos', 'Zacharie Aimilios', 'Nova Hogarth', 'Maja Ruoho', 'Uta Obando', 'Lidiya Szekeres', 'Chandra MacFarland', 'Szofi Granger', 'Camilla Holmström', 'Lilya Stainthorpe', 'Zofija Kendrick', 'Narelle Moon', 'Barbora MacLean', 'Alexandra Hisakawa', 'Alma María', 'Rosemary Okafor', 'Ige Behringer', 'Filip Traverse', 'Damjan Chapman', 'Wulf Carlevaro', 'Aaron Dreschner', 'Kumar Dahl', 'Eugenio Mataracı', 'Ferran Simen', 'Xavier Hayasaka', 'Luis Moray', 'Marcos Rudaski']
@@ -49,3 +47,14 @@ class AppFlags:
 # VITS
 #######################
 # 'ED\n': 0, 'p225': 1, 'p226': 2, 'p227': 3, 'p228': 4, 'p229': 5, 'p230': 6, 'p231': 7, 'p232': 8, 'p233': 9, 'p234'*: 10, 'p236': 11, 'p237': 12, 'p238': 13, 'p239': 14, 'p240': 15, 'p241': 16, 'p243': 17, 'p244': 18, 'p245': 19, 'p246': 20, 'p247': 21, 'p248': 22, 'p249': 23, 'p250': 24, 'p251': 25, 'p252': 26, 'p253': 27, 'p254': 28, 'p255': 29, 'p256': 30, 'p257': 31, 'p258': 32, 'p259': 33, 'p260': 34, 'p261': 35, 'p262': 36, 'p263': 37, 'p264': 38, 'p265': 39, 'p266': 40, 'p267': 41, 'p268': 42, 'p269': 43, 'p270': 44, 'p271': 45, 'p272': 46, 'p273'*: 47, 'p274': 48, 'p275': 49, 'p276': 50, 'p277': 51, 'p278': 52, 'p279': 53, 'p280': 54, 'p281': 55, 'p282': 56, 'p283': 57, 'p284': 58, 'p285': 59, 'p286': 60, 'p287': 61, 'p288': 62, 'p292': 63, 'p293': 64, 'p294': 65, 'p295': 66, 'p297': 67, 'p298': 68, 'p299': 69, 'p300': 70, 'p301': 71, 'p302': 72, 'p303': 73, 'p304': 74, 'p305': 75, 'p306': 76, 'p307': 77, 'p308': 78, 'p310': 79, 'p311': 80, 'p312': 81, 'p313': 82, 'p314': 83, 'p316': 84, 'p317': 85, 'p318': 86, 'p323': 87, 'p326': 88, 'p329': 89, 'p330': 90, 'p333': 91, 'p334': 92, 'p335': 93, 'p336': 94, 'p339': 95, 'p340': 96, 'p341': 97, 'p343': 98, 'p345': 99, 'p347': 100, 'p351': 101, 'p360': 102, 'p361': 103, 'p362': 104, 'p363': 105, 'p364': 106, 'p374': 107, 'p376': 108
+
+# English vocoder model
+# "vocoder_models/en/ljspeech/hifigan_v2"
+# "vocoder_models/universal/libri-tts/wavegrad"
+# "vocoder_models/en/vctk/hifigan_v2"
+# "vocoder_models/en/blizzard2013/hifigan_v2"
+
+# German vocoder model
+# "vocoder_models/de/thorsten/wavegrad"
+# "vocoder_models/de/thorsten/fullband-melgan"
+# "vocoder_models/de/thorsten/hifigan_v1"
--- utils/models.py
+++ utils/models.py
@@ -2,9 +2,10 @@ from TTS.api import TTS
 from IPython.display import Audio, display
 import torch
 import streamlit as st
+from utils.data import Database
 
 class TTSModel:
-    def __init__(self, model_data: str):
+    def __init__(self, model: str):
         """
         Initialize the TTSModel class.
         
@@ -12,60 +13,35 @@ class TTSModel:
         model (str): Model type.
         """
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        #self.check_model(model)
-        self.model_data = model_data
+        self.model_name = model
+        self.model_data = Database().get_model_data(model)
         self.load_model()
         
-
     def load_model(self):
-        
-        if self.model_data['lang'] == "en":
-            # English vocoder model
-            # "vocoder_models/en/ljspeech/hifigan_v2"
-            # "vocoder_models/universal/libri-tts/wavegrad"
-            # "vocoder_models/en/vctk/hifigan_v2"
-            # "vocoder_models/en/blizzard2013/hifigan_v2"
-            vocoder_model="vocoder_models/en/ljspeech/hifigan_v2"
-        if self.model_data['lang'] == "de":
-            # German vocoder model
-            # "vocoder_models/de/thorsten/wavegrad"
-            # "vocoder_models/de/thorsten/fullband-melgan"
-            # "vocoder_models/de/thorsten/hifigan_v1"
-            vocoder_model="vocoder_models/de/thorsten/hifigan_v1"
-        
-        
-        self.model = TTS(
-            model_name=self.model_data['path'],
-            vocoder_path=vocoder_model,
-            progress_bar=False
-            ).to(self.device)
+        model_full_name = self.model_data['path']
+        vocoder_model = self.model_data['vocoder']
 
+        if self.model_data['provider'] == 'coqui':
+            self.model = TTS(
+                model_name=model_full_name,
+                vocoder_path=vocoder_model,
+                progress_bar=False
+                ).to(self.device)
         
-        #     self.model = self.model.s("p230")
-                
     def convert(self,text):
         sound_file=f"./{self.model_data['name']}.wav"
         
+        model_config = {
+            "text":text,
+            "file_path":sound_file
+        }
+        
         if self.model.is_multi_lingual:
-            self.model.tts_to_file(
-                text,
-                speaker='Ana Florence',
-                language=self.model_data['lang'],
-                file_path=sound_file
-                )
-        elif self.model.is_multi_speaker:
-            self.model.tts_to_file(
-                text,
-                speaker='p234',
-                file_path=sound_file
-                )
-        else:
-            self.model.tts_to_file(
-                text,
-                file_path=sound_file
-                )
+            model_config["language"] = self.model_data['lang']
+        
+        if self.model.is_multi_speaker:
+            model_config["speaker"] = self.model_data['speaker']
+
+        self.model.tts_to_file(**model_config)
+        
         return sound_file
-    
-    # def check_model(self,model_name):
-    #     if model_name not in model_list:
-    #         raise(f"{model_name} is not supported.")
